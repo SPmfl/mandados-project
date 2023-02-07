673adad453e61959817e0f77c84c5269d9ac6c1a
SPmfl <matheo.franco.loaiza@gmail.com>
2023-02-07 13:00:47 -0500

    point mark crud working without validations

--- src/controllers/markPointControllers.js
+++ src/controllers/markPointControllers.js
@@ -1,36 +1,56 @@
 import Point from '../db/deb-models/pointModel.js';
 
-const getAllMarks = (req, res)=>{
-    res.status(200).json({
-        msg: "hola desde markpoint controller",
-        func: "getAllMarks"
-    });
+const getAllMarks = async (req, res)=>{
+    try {
+        const allMarks = await Point.find(); 
+        res.status(200).json(allMarks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "server error - server can not retrieve point marks"});
+    }
+
 }
-const getMark = (req, res)=>{
-    res.status(200).json({
-        msg: "hola desde markpoint controller",
-        func: `getMark ${req.params.id}`
-    });
+const getMark = async (req, res)=>{
+    try {
+        const mark = await Point.findById(req.params.id);
+        res.status(200).json(mark);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "server error - server can not retrieve point mark"});
+    }
 }
 
 const createMark = async (req, res)=>{
-    const {iduser,name,coordinates,comments,images} = req.body;
-    const newPoint = new Point({iduser,name,coordinates,comments,images});
-    const pointCreated = await newPoint.save();
-    res.status(200).json(pointCreated);
+    try {
+        const {iduser,name,coordinates,comments,images} = req.body;
+        const newPoint = new Point({iduser,name,coordinates,comments,images});
+        const pointCreated = await newPoint.save();
+        res.status(200).json(pointCreated);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "server error - point mark not created"});
+    }
 }
 
-const deleteMark = (req,res)=>{
-    res.status(200).json({
-        msg: "hola desde markpoint controller",
-        func: `getMark ${req.params.id}`
-    });
+const deleteMark = async (req,res)=>{
+    try {
+        await Point.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Point mark deleted"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "server error - point mark not deleted"});
+    }
 }
-const updateMark = (req,res)=>{
-    res.status(200).json({
-        msg: "hola desde markpoint controller",
-        func: `getMark ${req.params.id}`
-    });
+
+const updateMark = async (req,res)=>{
+    try {
+        const {iduser, name, coordinates, comments, images} = req.body;
+        const updatedMark = await Point.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(updatedMark);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "server error - point mark not updated"});
+    }
 }
 
 export {getAllMarks, getMark, createMark, deleteMark, updateMark}
