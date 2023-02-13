6c24d61547b36f03c822c9378c2ddfe1c5d75ffe
SPmfl <matheo.franco.loaiza@gmail.com>
2023-02-13 03:55:31 -0500

    using useEffect for loading markers in the map

--- src/components/cmp-map/cmpMap.jsx
+++ src/components/cmp-map/cmpMap.jsx
@@ -1,29 +1,37 @@
 import React, { useRef, useEffect, useState } from 'react';
+import ReactDOM from "react-dom";
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import './map.css';
+import './cmpMap.css';
+
+import geodata from './data.json';
 
 
 const apiKey = process.env.REACT_APP_MAPBOX_API_TOKEN;
-
 mapboxgl.accessToken = apiKey;
 
-export default function MapComponent() {
+
+export default function MapComponent(props) {
+
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-76.5165);
-  const [lat, setLat] = useState(3.4301);
-  const [zoom, setZoom] = useState(11);
+  const [lng, setLng] = useState(-76.5297);
+  const [lat, setLat] = useState(3.3756);
+  const [zoom, setZoom] = useState(15);
+  const [geoState, setGeoState] = useState(true);
+  const [features, setFeatures] = useState(geodata.features)
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
-      container: mapContainer.current,
+      container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
       zoom: zoom
     });
   });
 
+
+  /** update useState for coordinates on move */
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
     map.current.on('move', () => {
@@ -33,12 +41,44 @@ export default function MapComponent() {
     });
   });
 
+
+  useEffect(() => {
+    // Initialize the geolocate control.
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    });
+    // Add the control to the map.
+    if (geoState) {
+      map.current.addControl(geolocate);
+      setGeoState(false);
+    }
+    map.current.on('load', () => {
+      geolocate.trigger();
+    });
+  });
+
+  useEffect(() => {
+    addToMap(map.current, features);
+  })
+
+const addToMap = (map, features)=>{
+    features.map(feature =>{
+      new mapboxgl.Marker()
+      .setLngLat(feature.geometry.coordinates)
+      .addTo(map);
+    })
+}
+
   return (
     <div>
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-      <div ref={mapContainer} className="map-container" />
+      <div ref={mapContainer} className="map-container" id='map' />
     </div>
   );
 }
--- src/routes/map.jsx
+++ src/routes/map.jsx
@@ -1,13 +1,58 @@
 import MapComponent from "../components/cmp-map/cmpMap";
+import geojson from '../utils/geojson';
+import useSWR from 'swr';
+import '../styles/map.css';
+import axios from "axios";
+import { useEffect } from "react";
+import Feature from "../components/cmp-feature/feature";
 
 
+const lista = ["algo1", "algo1", "algo1", "algo1", "algo1", "algo1", "algo1"];
+
+
+/* make request for markers and distribute them to list container and map element */
+
+/**
+ * 
+ *  useEffect → bring markers data
+ *  put data in a useState
+ *  deploy a list of markers on list component
+ *  pass markers array to map component adding to map
+ */
+
+
+const fetcher = async (url)=> {
+    await axios.get(url, {
+        headers: {
+            'x_access_token': localStorage.getItem('x_access_token')
+        }
+    }).then(response => response.data)
+}
 
 function Map() {
-    return(
-        <div>
-            <MapComponent></MapComponent>
+
+    //const { data, error, isLoading } = useSWR('http://localhost:4500/api/operator/marks', fetcher);
+
+
+    useEffect(()=>{
+        console.log("hola maplist");
+    })
+    
+    console.log(geojson);
+
+    return (
+        <div id="maplist-container">
+
+            <div id="list-container" >
+                <div id="lista">{geojson.features.map(e => <Feature {...e}></Feature>  )}</div>
+            </div>
+
+            <div id="map-content">
+                <MapComponent markerList={geojson} ></MapComponent>
+            </div>
+
         </div>
-    );    
+    );
 }
 
 export default Map;
