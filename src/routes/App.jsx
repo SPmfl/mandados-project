2448a5712c60ae3fa780fff9cede4ee5ee12905a
SPmfl <matheo.franco.loaiza@gmail.com>
2023-02-12 21:19:08 -0500

    adding welcome sub route

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,28 +9,38 @@ import {
 } from 'react-router-dom';
 
 import Root from './root';
+import Login from './login'
+import Welcome from './welcome'
 import ErrorPage from '../utils/errorPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
+        element: <Login />,
+        errorElement: <ErrorPage />
+    }, 
+    {
+        path: '/app',
         element: <Root />,
         errorElement: <ErrorPage />,
         children: [
             {
-                path: 'map',
+                path: '/app',
+                element: <Welcome />
+            }, {
+                path: '/app/map',
                 element: <Map />
             }, {
-                path: 'profile',
+                path: '/app/profile',
                 element: <Profile />
             }
         ]
     }
-])
+]);
 
 function App() {
     return (
-        <RouterProvider router={ router }/>
+        <RouterProvider router={router} />
     );
 }
 
---
+++ src/routes/welcome.jsx
@@ -0,0 +1,18 @@
+import '../styles/welcome.css'
+function Welcome() {
+    return (
+        <div id="welcome-container">    
+            <a href="/app/map">
+            <span>
+                <h1>Welcome to Mandados APP!</h1>
+                <p>
+                    let's go!
+                </p>
+            </span>
+            </a>
+        </div>
+    );
+
+}
+
+export default Welcome;
