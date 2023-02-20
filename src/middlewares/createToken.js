7b1b2a26c2953b200f4e44f47f7ab3ee8d83bb30
SPmfl <matheo.franco.loaiza@gmail.com>
2023-02-20 15:18:56 -0500

    deleting or moving unused files

--- src/middlewares/createToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const createToken = (iduser)=>{
-    const token = jwt.sign(iduser, process.env.TOKEN_SECRET);
-    return token;
-}
-
-export default createToken;
