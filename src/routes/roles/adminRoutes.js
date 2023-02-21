b9c3cad78971ab3b9834683d605939fc6a32fdee
SPmfl <matheo.franco.loaiza@gmail.com>
2023-02-21 10:47:32 -0500

    deleting unused endpoints

--- src/routes/roles/adminRoutes.js
+++ src/routes/roles/adminRoutes.js
@@ -1,5 +1,4 @@
 import Router from 'express';
-// import * as markPointCtrl from '../../controllers/markPointControllers.js';
 import * as adminCtrl from '../../controllers/adminControllers.js';
 import passport from 'passport';
 import verifyRol from '../../middlewares/verifyRol.js';
@@ -11,12 +10,6 @@ router.get('/operators/:id', [passport.authenticate('jwt', { session: false }),v
 router.post('/operators', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], adminCtrl.createOperator);
 router.delete('/operators/:id', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], adminCtrl.deleteOperator);
 router.put('/operators/:id', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], adminCtrl.updateOperator);
-
-// router.get('/marks', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], markPointCtrl.getAllMarks);
-// router.get('/marks/:id', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], markPointCtrl.getMark);
-// router.post('/marks', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], markPointCtrl.createMark);
-// router.delete('/marks', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], markPointCtrl.deleteMark);
-// router.put('/marks/:id', [passport.authenticate('jwt', { session: false }),verifyRol.verifyAdmin], markPointCtrl.updateMark);
 
 
 export default router;
--- src/routes/roles/operatorRoutes.js
+++ src/routes/roles/operatorRoutes.js
@@ -1,5 +1,4 @@
 import Router from 'express';
-// import * as markPointCtrl from '../../controllers/markPointControllers.js';
 import * as operatorCtrl from '../../controllers/operatorControllers.js';
 import passport from 'passport';
 import verifyRol from '../../middlewares/verifyRol.js';
@@ -8,11 +7,5 @@ const router = Router();
 
 router.get('/profile', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], operatorCtrl.getInfo);
 router.put('/profile', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], operatorCtrl.updateInfo);
-
-// router.get('/marks', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], markPointCtrl.getAllMarks);
-// router.get('/marks/:id', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], markPointCtrl.getMark);
-// router.post('/marks/', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], markPointCtrl.createMark);
-// router.delete('/marks', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], markPointCtrl.deleteMark);
-// router.put('/marks/:id', [passport.authenticate('jwt', { session: false }),verifyRol.verifyOperator], markPointCtrl.updateMark);
 
 export default router;
