e28917531b47d0b50811efdc170ca09e8b5cef5d
SPmfl <matheo.franco.loaiza@gmail.com>
2023-03-11 13:14:57 -0500

    cleaned map with default drawing tools

--- src/components/cmp-map-2/cmpMap2.jsx
+++ src/components/cmp-map-2/cmpMap2.jsx
@@ -1,59 +1,82 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Map, { Marker, useControl } from 'react-map-gl';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
+import Map, { FullscreenControl, Marker, useControl, Popup, GeolocateControl } from 'react-map-gl';
 import * as turf from '@turf/turf';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
+
+import geojson from '../../utils/geojson';
 
 const apiKey = process.env.REACT_APP_MAPBOX_API_TOKEN;
 
 function CmpMap(params) {
     const mapRef = useRef();
-    const [zoom, setZoom] = useState(14);
-    const [viewState,setViewState] = useState({
-        longitude: -76.5316,
-        latitude: 3.3743
-    });
-    
-    useEffect(()=>{
-        if(!Map) return;
-        
-    }); 
 
+    const [lng, setLng] = useState(-76.5297);
+    const [lat, setLat] = useState(3.3756);
+    const [zoom, setZoom] = useState(15);
 
+    const initialView = {
+        longitude: lng,
+        latitude: lat,
+        zoom: zoom
+    }
 
+    const [features, setFeatures] = useState(geojson.features);
 
-    return(
+    const onMove = () => {
+        const map = mapRef.current;
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
+        });
+    }
+
+    function DrawControl(props) {
+        useControl(() => new MapboxDraw(props), {
+            position: props.position
+        });
+        return null;
+    }
+
+    return (
         <Map
-            {...viewState}
-            zoom={zoom}
-            onMove={evt => {
-                setViewState(evt.viewState);
-                setZoom(evt.target.getZoom().toFixed(2));
-            }}
-            style={{width: "100%", height: "80%"}}
-            mapStyle="mapbox://styles/mapbox/dark-v10"
+            ref={mapRef}
+            onMove={onMove}
+            initialViewState={initialView}
+            style={{ width: "100vw", height: "80vh" }}
+            mapStyle="mapbox://styles/mapbox/light-v9"
             mapboxAccessToken={apiKey}
         >
-            <DrawControl 
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <FullscreenControl position="bottom-left" />
+            <GeolocateControl position="bottom-left" />
+            <DrawControl
                 position="top-left"
-                displayControlsDefault={false}
-                controls={{
-                    polygon: true,
-                    trash: true
-                }}
-
+                displayControlsDefault={true}
+                // controls={{
+                //     line:true,
+                //     trash: true
+                //   }}
             />
+            {/* {features && features.map(ft => {
+                return <Marker longitude={ft.geometry.coordinates[0]}
+                    latitude={ft.geometry.coordinates[1]}
+                    onClick={e => {
+                        e.preventDefault();
+                        setSelected(ft);
+                        setShowPopUp(true);
+                    }} >
+                </Marker>
+            })
+            } */}
+
         </Map>
     )
 }
 
-function DrawControl(props){
-    useControl(() => new MapboxDraw(props),{
-        position: props.position
-    })
-}
 
-function drawCircle(latLong, rad){
-    return turf.circle({...latLong}, rad, {units:'miles'});
-}
+
 
 export default CmpMap;
